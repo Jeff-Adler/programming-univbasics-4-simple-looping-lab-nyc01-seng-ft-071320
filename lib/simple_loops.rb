6088fb600968bb6701ba4108feef124c1dbc0433
Jeff Adler <jeffm.adler@gmail.com>
2020-06-09 18:31:56 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -10,3 +10,11 @@ def loop_message_n_times(message, times)
     puts message
   end
 end
+
+def output_array(array)
+  counter = 0
+  while array[counter] do
+    puts array[counter]
+    counter += 1
+  end
+end
