4582dcf45e5dcfced774ba9c2221a0bba4453443
Jeff Adler <jeffm.adler@gmail.com>
2020-06-09 18:37:17 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -21,8 +21,9 @@ end
 
 def return_string_array(array)
   counter = 0
+  new_array = []
   while array[counter] do
-    array[counter] = counter.to_s
+    array[counter] = array[counter].to_s
     counter += 1
   end
   array
